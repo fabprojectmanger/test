339c9f5e627f0cf060216d0af884077b59ac008d
Ravin Kashyap <ravinwebsurgeon@gmail.com>
2022-03-04 13:06:13 +0530

    Added new content to Header and Footer Components.

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return (
+        <div>My name is Footer Component</div>
+    )
+}
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,7 @@
+const Header = () => {
+    return (
+        <div>My name is Header Component</div>
+    )
+}
+
+export default Header;
